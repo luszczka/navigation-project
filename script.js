66b01772177adfddf2cd21a92b038dfee77cc335
KAROLINA LUSZCZ <karolina.luszcz@sojournsolutions.com>
2022-05-13 23:42:46 +0200

    animation

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const mobileButton = document.querySelector(".navigation-menu");
 const mobileMenu = document.querySelector(".navigation-links");
+const burgerBar = document.querySelectorAll(".navigation-menu > span");
 
 mobileButton.addEventListener("click", function() {
     mobileMenu.classList.toggle("d-block");
+    for (let i = 0; i < burgerBar.length; i++) {
+        burgerBar[i].classList.toggle("burger-bar--clicked");
+    }
 });
